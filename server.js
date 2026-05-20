4677511eca885a2948d5fa8c60e51479454e5b1e
bbtechpro <billy.barber@gmail.com>
2026-05-20 16:23:54 -0400

    Created .env file and added PORT and MONG_URI connect string and added const dotenv = require('dotenv'); and dotenv.config();

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
-
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/basic-login-system';
